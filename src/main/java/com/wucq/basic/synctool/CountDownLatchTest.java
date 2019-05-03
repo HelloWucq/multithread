9de1690b9f14d069ccaa4b1d6297b5b7d7d7f821
wuchenqi <2455703859@qq.com>
2019-05-03 20:51:14 +0800

    同步工具类学习

--- src/main/java/com/wucq/basic/synctool/CountDownLatchTest.java
+++ src/main/java/com/wucq/basic/synctool/CountDownLatchTest.java
@@ -2,44 +2,51 @@ package com.wucq.basic.synctool;
 
 import java.util.concurrent.CountDownLatch;
 
+import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * CountDownLatchTest:可以用于确保某些活动直到其他活动都完成才继续执行
- */
 public class CountDownLatchTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(CountDownLatchTest.class);
+    private static final Logger logger=LoggerFactory.getLogger(CountDownLatchTest.class);
+    public static void main(String[] args) {   
 
-    public static void main(String[] args) throws InterruptedException {
-
-        final CountDownLatch countDownLatch = new CountDownLatch(1);
-        final CountDownLatch countDownLatch2 = new CountDownLatch(5);
-
-        for (int i = 0; i < 5; i++) {
-            Thread.sleep(1000);
-            new Thread(new Runnable() {
-
-                @Override
-                public void run() {
-                    try {
-                        countDownLatch.await();
-                        Thread.sleep(1000);
-                        countDownLatch2.countDown();
-                    } catch (InterruptedException e) {
-                        // TODO: handle exception
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-        }
-
-        long startTime = System.nanoTime();
-        countDownLatch.countDown();
-        countDownLatch2.await();
-        long stopTime = System.nanoTime();
-        logger.error("finish time pass {}", stopTime - startTime);
+        BasicConfigurator.configure();
+        final CountDownLatch latch = new CountDownLatch(2);
+         
+        new Thread(){
+            public void run() {
+                try {
+                    logger.info("Thread {} is running",Thread.currentThread().getName());
+                   Thread.sleep(000);
+                   logger.info("Thread {} finish running",Thread.currentThread().getName());
+                   latch.countDown();
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+            };
+        }.start();
+         
+        new Thread(){
+            public void run() {
+                try {
+                    logger.info("Thread {} is running",Thread.currentThread().getName());
+                    Thread.sleep(000);
+                    logger.info("Thread {} finish running",Thread.currentThread().getName());
+                    latch.countDown();
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+            };
+        }.start();
+         
+        try {
+            logger.info("waiting for the two child thread");
+           latch.await();
+           logger.info("two child thread finish");
+           logger.info("main thread continuing running");
+       } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
     }
-
 }
